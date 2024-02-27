aaa43c3af7b8e4aba04b95eab1d71e03270a3d82
Mingo07-dev <57068725+Mingo07-dev@users.noreply.github.com>
2024-02-27 12:24:07 +0100

    removed old auth

--- src/app/auth/oauth/model/idmroles.spec.ts
+++
@@ -1,7 +0,0 @@
-import { IDMRoles } from './idmroles';
-
-describe('IDMRoles', () => {
-  it('should create an instance', () => {
-    expect(new IDMRoles()).toBeTruthy();
-  });
-});
--- src/app/auth/oauth/model/idmuser.spec.ts
+++
@@ -1,7 +0,0 @@
-import { IDMUser } from './idmuser';
-
-describe('IDMUser', () => {
-  it('should create an instance', () => {
-    expect(new IDMUser()).toBeTruthy();
-  });
-});
