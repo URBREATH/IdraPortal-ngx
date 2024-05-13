b842ec5779356c5d47b875bc4831bbfe4a5beab4
Mingo07-dev <sajeva.marco01@gmail.com>
2024-05-13 09:37:00 +0200

    home started, tag cloud front end done

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { NbAccessChecker } from '@nebular/security';
-import { OwlOptions } from 'ngx-owl-carousel-o';
+import { DataCataglogueAPIService } from '../data-catalogue/services/data-cataglogue-api.service';
+import { ODMSCatalogueInfo } from '../data-catalogue/model/odmscatalogue-info';
+import { SearchRequest } from '../data-catalogue/model/search-request';
+import { SearchResult } from '../data-catalogue/model/search-result';
+import { randomInt } from 'crypto';
 
 @Component({
   selector: 'ngx-home',
@@ -9,29 +12,57 @@ import { OwlOptions } from 'ngx-owl-carousel-o';
 })
 export class HomeComponent implements OnInit {
 
-  customOptions: OwlOptions = {
-    loop: true,
-    autoplay:true,
-    autoHeight:true,
-    autoWidth:true,
-    dots:false,
-    center:true,
-    responsive: {
-      0: {
-        items:1
-      },
-      500: {
-        items:1
-      },
-      1000: {
-        items:1
-      }
-    }
-  }
+  constructor(private restApi:DataCataglogueAPIService) { }
 
-  constructor() { }
+  cataloguesInfos: Array<ODMSCatalogueInfo>=[]
+  totalDatasets:number=0;
+  tags: Array<any> = [];
+  searchResponse:SearchResult=new SearchResult();
+  searchRequest:SearchRequest=new SearchRequest();
 
   ngOnInit(): void {
+    this.restApi.getCataloguesInfo().subscribe(infos =>{
+      this.cataloguesInfos = infos;
+      this.searchRequest.nodes = infos.map(x=>x.id)
+      this.restApi.searchDatasets(this.searchRequest).subscribe(
+        res=>{
+            this.totalDatasets = res.count;
+            // let tags = res.facets.find(x=>x.displayName=="Tags").values;
+            // this.tags = tags.map(x=>{return {name:x.keyword, search_value:x.search_value}});
+            // let count = 0;
+            // this.tags = this.tags.slice(0,30);
+            // let a = document.getElementsByTagName('a');
+            // for (let index = 0; index < a.length; index++) {
+            //   a[index].setAttribute('data-weight',this.randomNumber(10).toString());
+            // }
+            // this.tags.forEach(element => {
+            //   if(count>=30) return;
+            //   count++;
+            //   let li = document.createElement('li');
+            //   let a = document.createElement('a');
+            //   a.innerHTML = element.name;
+            //   a.setAttribute('data-weight',this.randomNumber(10).toString());
+            //   a.setAttribute('href','#');
+            //   // a.onclick = ()=>{this.searchByTag(element.search_value)};
+            //   li.appendChild(a);
+            //   document.getElementById('tags').appendChild(li);
+            // });
+        },
+        err=>{
+          console.log(err);
+        });
+    },err=>{
+      console.log(err);
+    })
+  }
+
+  randomClass(){
+    let classes = ['tag-1','tag-2','tag-3','tag-4','tag-5','tag-6','tag-7','tag-8','tag-9','tag-10']
+    return classes[this.randomNumber(9)];
+  }
+
+  randomNumber(max:number){
+    return Math.floor(Math.random() * max + 1);
   }
 
 }
