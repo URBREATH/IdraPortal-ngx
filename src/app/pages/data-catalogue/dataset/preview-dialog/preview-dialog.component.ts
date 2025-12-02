c1ed194cfaa4058f3879df8b683428933bd433c3
fnudo <francesco.nudo@eng.it>
2025-12-02 15:39:39 +0100

    fix conflict

--- src/app/pages/data-catalogue/dataset/preview-dialog/preview-dialog.component.ts
+++ src/app/pages/data-catalogue/dataset/preview-dialog/preview-dialog.component.ts
@@ -19,97 +19,84 @@ export class PreviewDialogComponent {
   constructor(protected ref: NbDialogRef<PreviewDialogComponent>) {}
 
   ngOnInit() {
+    // --- CASE 1: Endpoint
     if (this.isEndpoint && this.endpointUrl) {
-      // Handle endpoint - nothing to do here, template will handle it
       this.loading = false;
+      return;
     }
-    else if (this.youtubeUrl) {
-      // Handle YouTube embed
+
+    // --- CASE 2: YouTube video
+    if (this.youtubeUrl) {
       this.loading = true;
-      let iframe = document.createElement('iframe');
+      const iframe = document.createElement('iframe');
       iframe.setAttribute('width', '560');
       iframe.setAttribute('height', '315');
       iframe.setAttribute('style', 'height: 70vh; width: 80vw; border: none;');
       iframe.setAttribute('title', 'YouTube video player');
       iframe.setAttribute('frameborder', '0');
-      // Use a limited set of permissions to reduce tracking-related errors
       iframe.setAttribute('allow', 'encrypted-media; picture-in-picture');
       iframe.setAttribute('loading', 'lazy');
       iframe.setAttribute('allowfullscreen', 'true');
       iframe.src = this.youtubeUrl;
-      
-      iframe.onload = (event: Event) => {
-        this.loading = false;
-      };
-      
-      iframe.onerror = (event: Event) => {
-        this.loading = false;
-      };
-      
-      document.getElementById('iframeBody').appendChild(iframe);
-    } 
-    else if(this.text == undefined){
-      this.loading = true;
-      let url = 'https://docs.google.com/gview?url='+this.url+'&embedded=true';
-      let iframe = document.createElement('iframe');
-      iframe.setAttribute('style','height: 70vh;width: 80vw;');
-      iframe.src = url;
-      let interval = setInterval(() => {
-        this.loading = true;
-        iframe.src = url;
-      }, 5000);
-      iframe.onload = (event: Event) => {
-        this.loading = false;
-      })
-      .catch(() => {
-        this.loading = false;
-      });
-  } else if (this.text == undefined) {
-    this.loading = true;
-    const src = 'https://docs.google.com/gview?url=' + this.url + '&embedded=true';
-    const iframe = document.createElement('iframe');
-    iframe.setAttribute('style','height: 70vh;width: 80vw;');
-    iframe.src = src;
 
-    const interval = setInterval(() => {
+      iframe.onload = () => this.loading = false;
+      iframe.onerror = () => this.loading = false;
+
+      document.getElementById('iframeBody')?.appendChild(iframe);
+      return;
+    }
+
+    // --- CASE 3: Show Google Doc (when text is undefined)
+    if (this.text === undefined) {
       this.loading = true;
+      const src = 'https://docs.google.com/gview?url=' + this.url + '&embedded=true';
+
+      const iframe = document.createElement('iframe');
+      iframe.setAttribute('style', 'height: 70vh;width: 80vw;');
       iframe.src = src;
-    }, 5000);
 
-    iframe.onload = () => {
-      this.loading = false;
-      clearInterval(interval);
-    };
-    iframe.onerror = () => {
-      this.loading = false;
-    };
+      const interval = setInterval(() => {
+        this.loading = true;
+        iframe.src = src;
+      }, 5000);
 
-    document.getElementById('iframeBody').appendChild(iframe);
-  // } else {
-  //   this.loading = true;
-  //   const pre = document.createElement('pre');
-  //   pre.setAttribute('style','height: 70vh;width: 80vw;overflow: auto;');
-  //   pre.textContent = this.text;
-  //   document.getElementById('iframeBody').appendChild(pre);
-  //   this.loading = false;
-  // }
-  } else {
-  this.loading = true;
-  const pre = document.createElement('pre');
-  pre.setAttribute('style','height: 70vh;width: 80vw;overflow: auto;');
-  pre.setAttribute('readonly','true');
+      iframe.onload = () => {
+        this.loading = false;
+        clearInterval(interval);
+      };
 
-  try {
-    const parsed = JSON.parse(this.text);
-    pre.textContent = JSON.stringify(parsed, null, 2);
-  } catch (e) {
-    // In caso non sia JSON valido, mostra così com'è
-    pre.textContent = this.text;
+      iframe.onerror = () => {
+        this.loading = false;
+      };
+
+      document.getElementById('iframeBody')?.appendChild(iframe);
+      return;
+    }
+
+    // --- CASE 4: Show raw text (JSON pretty-print if possible)
+    this.loading = true;
+    const pre = document.createElement('pre');
+    pre.setAttribute('style', 'height: 70vh;width: 80vw;overflow: auto;');
+    pre.setAttribute('readonly', 'true');
+
+    try {
+      const parsed = JSON.parse(this.text);
+      pre.textContent = JSON.stringify(parsed, null, 2);
+    } catch (e) {
+      pre.textContent = this.text;
+    }
+
+    document.getElementById('iframeBody')?.appendChild(pre);
+
+    this.loading = false;
   }
 
+  // --- FIX: method must be OUTSIDE ngOnInit()
   openEndpointInNewWindow() {
     if (this.endpointUrl) {
       window.open(this.endpointUrl, '_blank');
     }
   }
 }
+
+
